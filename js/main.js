76625d318b3ba18cd8d34c2713de434ff83e27e4
‪shehap Samer‬‏ <118848577+Shehapsamer@users.noreply.github.com>
2023-12-31 18:01:34 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,3 +15,14 @@ toglle_menu_large.addEventListener("click", function () {
     allssssss.classList.toggle("sssaaaaddff")
 })
 
+let toglle_menu = document.getElementById("toglle_menu");
+let nav_sidebar = document.getElementById("nav_sidebar");
+
+toglle_menu.addEventListener("click", function () {
+    nav_sidebar.classList.toggle("open_nav_sidebar")
+})
+
+function clearAll   () {
+    let clear = document.getElementById("clear");
+    clear.classList.add("clear_2")
+}
